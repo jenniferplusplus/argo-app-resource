56612f3d13916e4d8d278e91d0c1b58f1d2fc4ab
Jennifer Moore <jmoore@influxdata.com>
2023-11-21 14:50:10 -0600

    Initial project scaffolding

---
+++ pkg/driver/client.go
@@ -0,0 +1,44 @@
+package driver
+
+import (
+	"fmt"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/cluster"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/project"
+)
+
+type Connection struct {
+	Address string
+	Token   string
+}
+
+type Client struct {
+	projectClient project.ProjectServiceClient
+	clusterClient cluster.ClusterServiceClient
+}
+
+func NewClient(c *Connection) (*Client, error) {
+	apiClient, err := apiclient.NewClient(&apiclient.ClientOptions{
+		ServerAddr: fmt.Sprintf(c.Address),
+		Insecure:   true,
+		AuthToken:  c.Token,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	_, projectClient, err := apiClient.NewProjectClient()
+	if err != nil {
+		return nil, err
+	}
+
+	_, clusterClient, err := apiClient.NewClusterClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		projectClient: projectClient,
+		clusterClient: clusterClient,
+	}, nil
+}
---
+++ pkg/driver/project.go
@@ -0,0 +1,13 @@
+package driver
+
+import (
+	"context"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/project"
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+)
+
+func (c *Client) GetProject(name string) (*v1alpha1.AppProject, error) {
+	return c.projectClient.Get(context.Background(), &project.ProjectQuery{
+		Name: name,
+	})
+}
